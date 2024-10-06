2ecf9f4f7c038f3b57899486f87b516054b25735
Pradeep Bardewa <pradeepb@codeplateau.com>
2024-10-06 17:48:36 +0545

    Replace icons.

--- src/screens/Profile/styles.js
+++ src/screens/Profile/styles.js
@@ -39,7 +39,7 @@ export default StyleSheet.create({
     width: 100,
     height: 100,
     borderRadius: 50,
-    resizeMode: "cover",
+    resizeMode: "contain",
   },
   userName: {
     fontSize: 20,
